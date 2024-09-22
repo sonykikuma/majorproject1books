cd9e138c5cdcaeac9c484187ec86c0e55f1be399
sonykikuma <er.abhasoni@gmail.com>
2024-09-22 12:44:45 +0530

    changes in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,51 +1,94 @@
-import {useState} from 'react'
-import {NavLink, useNavigate} from 'react-router-dom'
+import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 
-const Header = ()=>{
-  const [searchTerm, setSearchTerm] = useState('');
+const Header = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const navigate = useNavigate();
 
-  const handleSearch = (e)=>{
+  const handleSearch = (e) => {
     e.preventDefault();
     navigate(`/productList/search?query=${encodeURIComponent(searchTerm)}`);
-  }
+  };
 
-  
-  return(
-  <header className='sticky-top'>
-    <nav className='navbar navbar-expand-lg '>
-      <div className="container">
-
-    <NavLink to="/" className="navbar-brand fw-bold text-danger">MyShoppingSite</NavLink>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className='nav-item'><NavLink className="nav-link" to="/productList/all">Product List</NavLink></li>
-          </ul>
-          <form className="d-flex" onSubmit={handleSearch}>
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-/>
-          </form>
-          {/* right side */}
-          <div className="d-flex ">
-            <NavLink to="/user" className='btn btn-secondary text-light '>Login</NavLink>
-          <NavLink to="/cart">  <div className='ps-2 pe-1'>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart" viewBox="0 0 16 16">
-            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-          </svg></div></NavLink>
-         <NavLink to="/wishlists">   <div className='ps-1'>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-heart" viewBox="0 0 16 16">
-            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15"/>
-          </svg></div></NavLink>
-            
+  return (
+    <header className="">
+      <nav className="navbar navbar-expand-lg ">
+        <div className="container">
+          <NavLink to="/" className="navbar-brand fw-bold text-primary">
+            MyShoppingSite
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-between"
+            id="navbarNav"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/productList/all">
+                  Product List
+                </NavLink>
+              </li>
+            </ul>
+            <form className="d-flex" onSubmit={handleSearch}>
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+            </form>
+            {/* right side */}
+            <div className="d-flex mt-1">
+              <NavLink to="/user" className="btn btn-primary text-light ">
+                Login
+              </NavLink>
+              <NavLink to="/cart">
+                {" "}
+                <div className="ps-3 pe-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="bi bi-cart "
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
+                  </svg>
+                </div>
+              </NavLink>
+              <NavLink to="/wishlists">
+                {" "}
+                <div className="ps-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="bi bi-heart"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+                  </svg>
+                </div>
+              </NavLink>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
-  </header>
-)}
-  export default Header
+      </nav>
+    </header>
+  );
+};
+export default Header;
