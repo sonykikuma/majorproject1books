fe11c9728255abdf08e17f8d834dd5b4167faac1
sonykikuma <er.abhasoni@gmail.com>
2024-12-02 16:06:47 +0530

    fix: message added for empty cart

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -79,85 +79,96 @@ const Cart = () => {
         {alertMessage && (
           <div className="alert alert-success">{alertMessage}</div>
         )}
-        <div className="row">
-          <div className="col-md-8">
-            {cart.items?.map((item) => (
-              <div className=" " key={item?._id}>
-                <div className="card mb-3">
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="m-1  col-md-6 text-center">
-                      <div className="py-1">
-                        <img
-                          className="img-fluid "
-                          src={item?.productId?.coverImageUrl}
-                          alt="cover image"
-                          style={{
-                            objectFit: "cover",
-                            height: "300px",
-                            width: "250px",
-                          }}
-                        />
+        {cart.items?.length === 0 ? (
+          <div className="text-center my-5">
+            <h4>Your cart is empty!</h4>
+            <p>Please add items to your cart.</p>
+          </div>
+        ) : (
+          <div className="row">
+            <div className="col-md-8">
+              {cart.items?.map((item) => (
+                <div className=" " key={item?._id}>
+                  <div className="card mb-3">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div className="m-1  col-md-6 text-center">
+                        <div className="py-1">
+                          <img
+                            className="img-fluid "
+                            src={item?.productId?.coverImageUrl}
+                            alt="cover image"
+                            style={{
+                              objectFit: "cover",
+                              height: "300px",
+                              width: "250px",
+                            }}
+                          />
+                        </div>
+                        <div className="">
+                          <strong>{item?.productId?.title}</strong>
+                          <br />
+                          {item?.productId?.author}
+                        </div>
                       </div>
-                      <div className="">
-                        <strong>{item?.productId?.title}</strong>
-                        <br />
-                        {item?.productId?.author}
-                      </div>
-                    </div>
 
-                    <div className="m-1  col-md-6">
-                      <p>Price: Rs.{item?.productId?.price}</p>
-                      <p>
-                        {" "}
-                        Quantity: {item?.quantity}{" "}
-                        <button
-                          onClick={() => handleIncrement(item?.productId?._id)}
-                        >
+                      <div className="m-1  col-md-6">
+                        <p>Price: Rs.{item?.productId?.price}</p>
+                        <p>
                           {" "}
-                          +{" "}
-                        </button>{" "}
-                        <button
-                          onClick={() => handleDecrement(item?.productId?._id)}
-                        >
-                          {" "}
-                          -{" "}
-                        </button>
-                      </p>
-                      <div className="">
-                        <button
-                          onClick={() =>
-                            movetowishlistHandler(item?.productId?._id)
-                          }
-                          className="btn btn-primary"
-                        >
-                          Move to wishlist
-                        </button>
-                        <button
-                          onClick={() => deleteHandler(item?.productId?._id)}
-                          className="ms-2 btn btn-danger mt-1"
-                        >
-                          delete
-                        </button>
+                          Quantity: {item?.quantity}{" "}
+                          <button
+                            onClick={() =>
+                              handleIncrement(item?.productId?._id)
+                            }
+                          >
+                            {" "}
+                            +{" "}
+                          </button>{" "}
+                          <button
+                            onClick={() =>
+                              handleDecrement(item?.productId?._id)
+                            }
+                          >
+                            {" "}
+                            -{" "}
+                          </button>
+                        </p>
+                        <div className="">
+                          <button
+                            onClick={() =>
+                              movetowishlistHandler(item?.productId?._id)
+                            }
+                            className="btn btn-primary"
+                          >
+                            Move to wishlist
+                          </button>
+                          <button
+                            onClick={() => deleteHandler(item?.productId?._id)}
+                            className="ms-2 btn btn-danger mt-1"
+                          >
+                            delete
+                          </button>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
+              ))}
+            </div>
+            <div className="col-md-4">
+              <div className=" ">
+                <Address />
               </div>
-            ))}
-          </div>
-          <div className="col-md-4">
-            <div className=" ">
-              <Address />
-            </div>
-            <div className="">
-              {" "}
-              <PriceDetails
-                totalQuantity={totalQuantity}
-                totalPrice={totalPrice}
-              />
+              <div className="">
+                {" "}
+                <PriceDetails
+                  totalQuantity={totalQuantity}
+                  totalPrice={totalPrice}
+                />
+              </div>
             </div>
           </div>
-        </div>
+        )}
         {/* <div className="container d-flex justify-content-between ">
           <div className="col-md-4 ">
             <Address />
